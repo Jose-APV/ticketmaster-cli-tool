a5dc25c24e73e1da9132b4d757c541a40a778239
Jose <joseportillo0823@gmail.com>
2025-03-27 23:19:01 -0700

    Add Exit as the first option in list

--- app.js
+++ app.js
@@ -21,20 +21,23 @@ const _keywordHistoryPrompt = async (keywords) => {
     const displayKeywords = keywords.map((keyword) => {
         return {name: keyword.keyword, value: keyword.keyword}
     });
-
-    return await select({
+    displayKeywords.unshift({ name: 'Exit', value: 'exit' }); // allows exit to be the first option
+    return await rawlist({
         message: 'Select a previous keyword to navigate to: ',
         choices: displayKeywords
     });
 }
 
-export const keywordHistory = async () => {
+export const keywordHistory = async (args) => {
     // retrieve all keywords
     const keywordCollections = await db.find('search_history_keyword');
 
     // List prompt all previous keywords
     const keywordSelected = await _keywordHistoryPrompt(keywordCollections);
-
-    // redirect user to whatever the user selects
-    searchEvent({keyword: keywordSelected});
+    if(keywordSelected === 'exit') {
+        return;
+    }else{
+        // redirect user to whatever the user selects
+        searchEvent({keyword: keywordSelected});
+    }
 }
